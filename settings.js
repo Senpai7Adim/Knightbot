51b11473a38e745c49fb8a5b96c5650fb03a0985
Senpai7Adim <ifoumiguel@gmail.com>
2025-10-09 10:28:49 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Senp@! Bot",
+  botOwner: 'SENPAI', // Your name
+  ownerNumber: '237675183007', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
